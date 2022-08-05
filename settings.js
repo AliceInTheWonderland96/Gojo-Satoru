099333d4cd1e5383a63b1aa7a6d2cab8cd372f7c
AliceInTheWonderland96 <60828857+AliceInTheWonderland96@users.noreply.github.com>
2022-08-06 01:02:15 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,7 +15,7 @@ global.APIs = {
 
 //Api Key\\
 global.APIKeys = {
-	'https://zenzapis.xyz': 'sanzychan01', //credit goes to sanzy yt, if api key expire, buy it and put it here?\\
+	'https://zenzapis.xyz': '2b956cf00e', //credit goes to sanzy yt, if api key expire, buy it and put it here?\\
 }
 
 //customize settings\\
